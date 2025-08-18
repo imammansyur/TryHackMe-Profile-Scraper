02f61639722701f1b507ac56d6224a22c05cd10a
Imam Mansyur Solichin <56965041+imammansyur@users.noreply.github.com>
2025-08-18 19:48:41 +0700

    Create update-readme.sh

---
+++ update-readme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change to your github repo details
+git clone https://github.com/imammansyur/SecOps-Learning.git
+cd SecOps-Learning
+
+python ../thm-scraper.py
+
+git add TryHackMe/README.md
+git commit -m "Update THM Progress"
+git push
